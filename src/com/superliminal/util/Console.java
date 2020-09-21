31a8baaefcab091c33d639f215492377d06ce182
Don Hatch <hatch@plunk.org>
2020-09-21 04:52:01 -0700

    Re-enable the debug console part of the debug console window,
    and add skeleton of experimental new controls UI,
    and some other gratuitous stuff in little MC4DView windows.

--- src/com/superliminal/util/Console.java
+++ src/com/superliminal/util/Console.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -11,20 +13,33 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
+import javax.swing.border.TitledBorder;
 
 import com.superliminal.magiccube4d.MC4DView;
 import com.superliminal.magiccube4d.MC4DView.ItemCompleteCallback;
 import com.superliminal.magiccube4d.MagicCube;
 import com.superliminal.magiccube4d.PuzzleManager;
 import com.superliminal.magiccube4d.RotationHandler;
+
+import com.superliminal.util.PropControls.PropCheckBox;
+import com.superliminal.util.PropControls.PropRadioButton;
 
 /**
  * Manages a singleton JFrame console. When visible, stdout and stderr streams are redirect to it.
@@ -41,6 +56,38 @@ public class Console {
     private final static PrintStream mOriginalOut = System.out;
     private final static PrintStream mOriginalErr = System.err;
     private static Component dependent = null; // Call its repaint method as needed.
+    // a component that simply forwards all repaint calls to the current dependent object.
+    private static Component mRepainter = new Component() {
+        @Override
+        public void repaint() {
+            if(dependent != null)
+                dependent.repaint();
+        }
+    };
+    // TODO: get rid of this when PropRadioButton can accept dependent=null without throwing
+    private static Component mDummyRepainter = new Component() { @Override public void repaint() {}; };
+
+    private static void setEnabledWhenPropertyIsTrue(final Component c, final String propname, final boolean dflt) {
+        PropertyManager.top.addPropertyListener(new PropertyManager.PropertyListener() {
+            @Override
+            public void propertyChanged(String property, String newval) {
+                c.setEnabled(PropertyManager.getBoolean(propname, dflt));
+            }
+        }, propname);
+        c.setEnabled(PropertyManager.getBoolean(propname, dflt));
+    }
+
+    private static Component multiHorizontalSplitPane(final Component[] children, final int startIndex) {
+      if (startIndex == children.length-1) return children[startIndex];
+      return new JSplitPane(
+          /*orientation= */JSplitPane.HORIZONTAL_SPLIT,
+          /*continuoutLayout= */true,
+          /*leftComponent= */children[startIndex],
+          /*rightComponent= */multiHorizontalSplitPane(children, startIndex+1)) {{
+          setResizeWeight(1./(children.length-startIndex));
+      }};
+    }
+
     static {
         mTextArea.setWrapStyleWord(true);
         JScrollPane scroller = new JScrollPane(mTextArea);
@@ -55,35 +102,103 @@ public class Console {
         });
         JMenuBar menubar = new JMenuBar();
         mInstance.setJMenuBar(menubar);
-        JMenu menu = new JMenu("Don's menu");
+        JMenu menu = new JMenu("Experimental new puzzle menu");
         menubar.add(menu);
         menu.add(new JMenuItem("Click me please!"));
-        Container contents = mInstance.getContentPane();
-        contents.setLayout(new BorderLayout());
-        contents.add(makePuzzle(), "Center"); // Just for example. Needs the scroller below but they fight for space.
-//      contents.add(scroller, "South");
+
+        class JRow extends JPanel {{ setLayout(new BoxLayout(this, BoxLayout.X_AXIS)); }};
+        class JCol extends JPanel {{ setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); }
+            public void addLeftJustified(final int indentPixels, final Component c) {
+                add(new JRow() {{
+                    if (indentPixels != 0) add(Box.createHorizontalStrut(indentPixels));
+                    add(c);
+                    add(Box.createHorizontalGlue());
+                }});
+            }
+        };
+        mInstance.getContentPane().add(new JSplitPane(
+            /*orientation= */JSplitPane.VERTICAL_SPLIT,
+            /*continuoutLayout= */true,
+            /*topComponent= */new JSplitPane(
+                /*orientation= */JSplitPane.HORIZONTAL_SPLIT,
+                /*continuousLayout= */true,
+
+                /*leftComponent= */new JCol() {{
+                    add(new JCol() {{
+                        setBorder(new TitledBorder("EXPERIMENTAL NEW CONTROLS"));
+                        // TODO: investigate why all the views are getting repainted before the controls get enabled/disabled!  that's not friendly!
+                        addLeftJustified(0, new PropCheckBox("Allow Antialiasing", "antialiasing", true, mRepainter, "Whether to smooth polygon edges when still - Warning: Can be expensive on large puzzles"));
+                        final JRadioButton whenstill = new PropRadioButton("when still", "antialiasingmeansalways", /*dflt=*/false, /*invert=*/true, mDummyRepainter, "Antialias only when no animation is in progress - Warning: Can interfere with interaction on large puzzles");
+                        final JRadioButton always = new PropRadioButton("always (NOT HOOKED UP YET: AWAITING CODE REVIEW)", "antialiasingmeansalways", /*dflt=*/false, /*invert=*/false, mDummyRepainter, "Antialias every frame - Warning: can be very slow on large puzzles");
+                        setEnabledWhenPropertyIsTrue(whenstill, "antialiasing", true);
+                        setEnabledWhenPropertyIsTrue(always, "antialiasing", true);
+                        new ButtonGroup() {{
+                            add(whenstill);
+                            add(always);
+                        }};
+                        addLeftJustified(30, whenstill);
+                        addLeftJustified(30, always);
+                        addLeftJustified(0, new PropCheckBox("Use back buffer (NOT HOOKED UP YET: AWAITING CODE REVIEW)", "useyetanotherbackbuffer", false, mRepainter, "Whether to use yet another back buffer.  Seems to speed up antialiasing tremendously on some platforms."));
+                        addLeftJustified(0, new JButton("Just repaint the main view") {{
+                            addActionListener(new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent ae) {
+                                    dependent.repaint();
+                                }
+                            });
+                        }});
+                        setMaximumSize(getPreferredSize());  // so the controls don't spread out when the panel is stretched vertically
+                    }});
+                }},
+
+                /*rightComponent= */multiHorizontalSplitPane(
+                    new Component[] {
+                        makePuzzle("{3,3}x{}", "3(4)"),
+                        makePuzzle("{4,3}x{}", "3"),
+                        makePuzzle("{3,4}x{}", "3(4)"),
+                        makePuzzle("{5,3}x{}", "3(2.5)"),
+                        makePuzzle("{3,5}x{}", "3(4)"),
+                    }, 0)
+                ),
+            /*bottomComponent= */scroller));
     }
 
-    private static Component makePuzzle() {
-        final String SCHLAFLI = "{3,3,3}";
-        final String LENGTHSTRING = "3";
+    private static Component makePuzzle(String schlafli, String lengthString) {
         final int[] num_twists = new int[1];
         System.out.println("version " + System.getProperty("java.version"));
-        final MC4DView view = new MC4DView(new PuzzleManager(SCHLAFLI, LENGTHSTRING, new JProgressBar()), new RotationHandler());
-        view.addStickerListener(new MC4DView.StickerListener() {
-            @Override
-            public void stickerClicked(InputEvent e, MagicCube.TwistData twisted) {
-                view.animate(twisted, new ItemCompleteCallback() {
+        final MC4DView view = new MC4DView(new PuzzleManager(schlafli, lengthString, new JProgressBar()), new RotationHandler()) {{
+            // TODO: MC4DView should listen to all of these!!
+            for (final String propname : new String[] {MagicCube.DEBUGGING,
+                                                       MagicCube.BLINDFOLD,
+                                                       "antialiasing",
+                                                       "shadows",
+                                                       "useyetanotherbackbuffer",
+                                                      }) {
+                PropertyManager.top.addPropertyListener(new PropertyManager.PropertyListener() {
                     @Override
-                    public void onItemComplete(MagicCube.TwistData twist) {
-                        System.out.println("Num Twists: " + ++num_twists[0]);
+                    public void propertyChanged(String property, String newval) {
+                        repaint();
                     }
-                });
-                boolean is_checked = PropertyManager.getBoolean(MagicCube.BLINDFOLD, false);
-                PropertyManager.userprefs.setProperty(MagicCube.BLINDFOLD, !is_checked + "");
-                dependent.repaint();
-            }
-        });
+                }, propname);
+            };
+        }};
+        if (false)  // this is good example code but it's surprising so don't do it
+        {
+            view.addStickerListener(new MC4DView.StickerListener() {
+                @Override
+                public void stickerClicked(InputEvent e, MagicCube.TwistData twisted) {
+                    view.animate(twisted, new ItemCompleteCallback() {
+                        @Override
+                        public void onItemComplete(MagicCube.TwistData twist) {
+                            System.out.println("Num Twists: " + ++num_twists[0]);
+                        }
+                    });
+                    boolean is_checked = PropertyManager.getBoolean(MagicCube.BLINDFOLD, false);
+                    PropertyManager.userprefs.setProperty(MagicCube.BLINDFOLD, !is_checked + "");
+                    dependent.repaint();
+                }
+            });
+        }
         return view;
     }
 
