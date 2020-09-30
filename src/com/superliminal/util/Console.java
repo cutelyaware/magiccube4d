9cc43603eb73d2af59c77a845f11c3ad82eccf85
Don Hatch <hatch@plunk.org>
2020-09-29 19:47:56 -0700

    Revert "Added some example content to the debugging console for Don including a"
    This reverts commit d189900514eef235d3f9d3dd76227195296b1369.

--- src/com/superliminal/util/Console.java
+++ src/com/superliminal/util/Console.java
@@ -1,10 +1,6 @@
 package com.superliminal.util;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.Toolkit;
-import java.awt.event.InputEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -12,19 +8,9 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
-
-import com.superliminal.magiccube4d.MC4DView;
-import com.superliminal.magiccube4d.MC4DView.ItemCompleteCallback;
-import com.superliminal.magiccube4d.MagicCube;
-import com.superliminal.magiccube4d.PuzzleManager;
-import com.superliminal.magiccube4d.RotationHandler;
 
 /**
  * Manages a singleton JFrame console. When visible, stdout and stderr streams are redirect to it.
@@ -40,12 +26,12 @@ public class Console {
     private final static PrintStream mTextStream = new PrintStream(new TextAreaOutputStream(mTextArea));
     private final static PrintStream mOriginalOut = System.out;
     private final static PrintStream mOriginalErr = System.err;
-    private static Component dependent = null; // Call its repaint method as needed.
     static {
         mTextArea.setWrapStyleWord(true);
         JScrollPane scroller = new JScrollPane(mTextArea);
-        int console_height = 500;
+        int console_height = 300;
         mInstance.setBounds(0, (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight() - console_height, 1000, console_height);
+        mInstance.getContentPane().add(scroller);
         mInstance.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -53,53 +39,15 @@ public class Console {
                 System.setErr(mOriginalErr);
             }
         });
-        JMenuBar menubar = new JMenuBar();
-        mInstance.setJMenuBar(menubar);
-        JMenu menu = new JMenu("Don's menu");
-        menubar.add(menu);
-        menu.add(new JMenuItem("Click me please!"));
-        Container contents = mInstance.getContentPane();
-        contents.setLayout(new BorderLayout());
-        contents.add(makePuzzle(), "Center"); // Just for example. Needs the scroller below but they fight for space.
-//      contents.add(scroller, "South");
     }
 
-    private static Component makePuzzle() {
-        final String SCHLAFLI = "{3,3,3}";
-        final String LENGTHSTRING = "3";
-        final int[] num_twists = new int[1];
-        System.out.println("version " + System.getProperty("java.version"));
-        final MC4DView view = new MC4DView(new PuzzleManager(SCHLAFLI, LENGTHSTRING, new JProgressBar()), new RotationHandler());
-        view.addStickerListener(new MC4DView.StickerListener() {
-            @Override
-            public void stickerClicked(InputEvent e, MagicCube.TwistData twisted) {
-                view.animate(twisted, new ItemCompleteCallback() {
-                    @Override
-                    public void onItemComplete(MagicCube.TwistData twist) {
-                        System.out.println("Num Twists: " + ++num_twists[0]);
-                    }
-                });
-                boolean is_checked = PropertyManager.getBoolean(MagicCube.BLINDFOLD, false);
-                PropertyManager.userprefs.setProperty(MagicCube.BLINDFOLD, !is_checked + "");
-                dependent.repaint();
-            }
-        });
-        return view;
-    }
+    private Console() {}
 
-    private Console() {
-    }
-
-    public static void show(String title, Component dep) {
-        dependent = dep;
+    public static void show(String title) {
         mInstance.setTitle(title);
         System.setOut(mTextStream);
         System.setErr(mTextStream);
         mInstance.setVisible(true);
-    }
-
-    public static void show(String title) {
-        show(title, null);
     }
 
     public static int getLineCount() {
@@ -137,12 +85,10 @@ public class Console {
         }
 
         @Override
-        public void flush() {
-        }
+        public void flush() {}
 
         @Override
-        public void close() {
-        }
+        public void close() {}
 
         @Override
         public void write(int b) throws IOException {
